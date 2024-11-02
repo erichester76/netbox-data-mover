fb5122192a93c9ae505ef5cbe017cf7df4c5db3e
Eric Hester <eric.hester@umbrella.tech>
2024-11-01 22:40:55 -0400

    cleaning up structure

--- netbox-data-mover/__init__.py
+++ netbox-data-mover/__init__.py
@@ -1,13 +1,13 @@
 
-from extras.plugins import PluginConfig
+from netbox.plugins import PluginConfig
 
 class NetboxDataMoverConfig(PluginConfig):
-    name = "netbox_data_mover"
+    name = "netbox-data-mover"
     verbose_name = "NetBox Data Mover"
     description = "A NetBox plugin to manage data movement configurations between various sources and destinations."
-    version = "0.1"
+    version = "0.0.1"
     author = "Eric Hester"
     author_email = "hester1@clemson.edu"
-    base_url = "netbox_data_mover"
+    base_url = "datamover"
 
 config = NetboxDataMoverConfig
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="netbox_data_mover",
-    version="0.1",
+    version="0.0.1",
     description="A NetBox plugin to manage data movement configurations between various sources and destinations.",
     author="Eric Hester",
     author_email="hester1@clemson.edu",
