c83e5f2adb05171a65fd3df2c4ff5598c11ce01e
Eric Hester <eric.hester@umbrella.tech>
2024-11-02 17:05:47 -0400

    added missing as_view

--- netbox_data_mover/urls.py
+++ netbox_data_mover/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add', views.DataMoverEditView.as_view(), name='datamover_add'),
     path('<int:pk>/edit', views.DataMoverEditView.as_view(), name='datamover_edit'),
     path('<int:pk>/delete', views.DataMoverDeleteView.as_view(), name='datamover_delete'),
-    path('<int:pk>/', views.DataMoverDetailView, name='datamover_detail'),
+    path('<int:pk>/', views.DataMoverDetailView.as_view(), name='datamover_detail'),
     path('<int:pk>/trigger/', views.DataMoverConfigTriggerJobView.as_view(), name='datamover_trigger_job'),
 ]
