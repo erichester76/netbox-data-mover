03a35488c23647eff4c0982bf1218264c2ddebde
Eric Hester <eric.hester@umbrella.tech>
2024-11-02 19:46:42 -0400

    .

--- netbox_data_mover/tables.py
+++ netbox_data_mover/tables.py
@@ -3,11 +3,19 @@ from .models import DataMoverConfig, DataMoverDataSource
 from netbox.tables import NetBoxTable
 
 class DataMoverConfigTable(NetBoxTable):
+    name = tables.Column(linkify=True)
+    source = tables.Column(linkify=True)
+    destination = tables.Column(linkify=True)
+ 
+    
     class Meta:
         model = DataMoverConfig
         fields = ('name', 'description', 'schedule', 'source', 'destination', 'last_run_status', 'last_run_records_changed', 'last_run_time')
 
 class DataMoverDataSourceTable(NetBoxTable):
+    name = tables.Column(linkify=True)
+
+    
     class Meta:
         model = DataMoverDataSource
-        fields = ('name', 'type', 'api_url')
+        fields = ('name', 'type', 'api_url', 'auth_details')
