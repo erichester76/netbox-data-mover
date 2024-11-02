3ccdf6c279b39046a6214c59b06d4bf6eb050d9a
Eric Hester <eric.hester@umbrella.tech>
2024-11-01 22:45:30 -0400

    typo

--- netbox-data-mover/__init__.py
+++ netbox-data-mover/__init__.py
@@ -2,7 +2,7 @@
 from netbox.plugins import PluginConfig
 
 class NetboxDataMoverConfig(PluginConfig):
-    name = "netbox-data-mover"
+    name = "netbox_data_mover"
     verbose_name = "NetBox Data Mover"
     description = "A NetBox plugin to manage data movement configurations between various sources and destinations."
     version = "0.0.1"
