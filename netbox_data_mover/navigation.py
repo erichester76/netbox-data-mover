22c1bfa1435398d95e1a91afd48e79b2954396b3
Eric Hester <eric.hester@umbrella.tech>
2024-11-02 17:09:32 -0400

    fixed reverseurl

--- netbox_data_mover/navigation.py
+++ netbox_data_mover/navigation.py
@@ -3,7 +3,7 @@ from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_data_mover:index',
+        link='plugins:netbox_data_mover:datamover_list',
         link_text='Data Mover Configurations',
         buttons=()
     ),
