77506780bdf26c0c2adb92b49d11bc64d7448cfb
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 14:36:45 -0500

    backwards

--- netbox_data_mover/api/serializers.py
+++ netbox_data_mover/api/serializers.py
@@ -5,9 +5,9 @@ from netbox.api.serializers import NetBoxModelSerializer
 class DataMoverDataSourceSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverDataSource
-        fields = ['id', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
+        fields = ['id', 'name', 'type', 'module', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
 
 class DataMoverConfigSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverConfig
-        fields = ['id', 'name', 'type', 'module', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
+        fields = ['id', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
