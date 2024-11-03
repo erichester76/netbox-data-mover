d985779b2a325107fe076a462bc32feec40cb900
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 12:43:33 -0500

    added warning

--- netbox_data_mover/views.py
+++ netbox_data_mover/views.py
@@ -2,11 +2,17 @@ from netbox.views import generic
 from .models import DataMoverConfig, DataMoverDataSource
 from .forms import DataMoverConfigForm, DataMoverDataSourceForm
 from .tables import DataMoverConfigTable, DataMoverDataSourceTable
+from django.contrib import messages
 
 class DataMoverConfigListView(generic.ObjectListView):
     queryset = DataMoverConfig.objects.all()
     table = DataMoverConfigTable
     permission_required = 'netbox_data_mover.view_datamoverconfig'
+    
+    def get(self, request, *args, **kwargs):
+        if not DataMoverDataSource.objects.exists():
+            messages.warning(request, "No Data Sources available. Please add a <a href='{% url \"plugins:netbox_data_mover:datamoverdatasource_add\" %}'>Data Source</a> before creating a Data Mover Config.")
+        return super().get(request, *args, **kwargs)
 
 class DataMoverConfigDetailView(generic.ObjectView):
     queryset = DataMoverConfig.objects.all()
