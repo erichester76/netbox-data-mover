44fee45ce817520ee6496c4f247e197537d37bb2
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 14:57:01 -0500

    .

--- netbox_data_mover/urls.py
+++ netbox_data_mover/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # DataMoverConfig URLs
     path('datamoverconfigs/', views.DataMoverConfigListView.as_view(), name='datamoverconfig_list'),
     path('datamoverconfigs/add/', views.DataMoverConfigEditView.as_view(), name='datamoverconfig_add'),
-    path('datamoverconfigs/import/', views.DataMoverConfigBulkImportView.as_view(), name='datamoverconfig_import'),
+    path('datamoverconfigs/import/', views.DataMoverConfigBulkImportView.as_view(), name='datamoverconfig_bulk_import'),
     path('datamoverconfigs/edit/', views.DataMoverConfigBulkEditView.as_view(), name='datamoverconfig_bulk_edit'),
     path('datamoverconfigs/delete/', views.DataMoverConfigBulkDeleteView.as_view(), name='datamoverconfig_bulk_delete'),
     path('datamoverconfigs/<int:pk>/', views.DataMoverConfigDetailView.as_view(), name='datamoverconfig_detail'),
@@ -17,7 +17,7 @@ urlpatterns = [
     # DataMoverDataSource URLs
     path('datamoverdatasources/', views.DataMoverDataSourceListView.as_view(), name='datamoverdatasource_list'),
     path('datamoverdatasources/add/', views.DataMoverDataSourceEditView.as_view(), name='datamoverdatasource_add'),
-    path('datamoverdatasources/import/', views.DataMoverDataSourceBulkImportView.as_view(), name='datamoverdatasource_import'),
+    path('datamoverdatasources/import/', views.DataMoverDataSourceBulkImportView.as_view(), name='datamoverdatasource_bulk_import'),
     path('datamoverdatasources/edit/', views.DataMoverDataSourceBulkEditView.as_view(), name='datamoverdatasource_bulk_edit'),
     path('datamoverdatasources/delete/', views.DataMoverDataSourceBulkDeleteView.as_view(), name='datamoverdatasource_bulk_delete'),
     path('datamoverdatasources/<int:pk>/', views.DataMoverDataSourceDetailView.as_view(), name='datamoverdatasource_detail'),
