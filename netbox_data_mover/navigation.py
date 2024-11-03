c975b99ae2d8af4537db8a9370132d022ede9501
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 14:52:30 -0500

    .

--- netbox_data_mover/navigation.py
+++ netbox_data_mover/navigation.py
@@ -3,16 +3,16 @@ from netbox.plugins import PluginMenuItem, PluginMenuButton
 
 menu_items = (
      PluginMenuItem(
-        link="plugins:netbox_data_mover:datamover_list",
+        link="plugins:netbox_data_mover:datamoverconfig_list",
         link_text="Movers",
         buttons=[
             PluginMenuButton(
-                link="plugins:netbox_data_mover:datamover_add",
+                link="plugins:netbox_data_mover:datamoverconfig_add",
                 title="Add",
                 icon_class="mdi mdi-plus-thick",
             ),
             PluginMenuButton(
-                link="plugins:netbox_data_mover:datamover_bulk_import",
+                link="plugins:netbox_data_mover:datamoverconfig_bulk_import",
                 title="Import",
                 icon_class="mdi mdi-upload",
             ),
