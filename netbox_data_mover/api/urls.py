4f3dd57ee32600da3640c62b8b0b0790768b0553
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 14:04:40 -0500

    fixed hyperlink

--- netbox_data_mover/api/urls.py
+++ netbox_data_mover/api/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'datasources', views.DataMoverDataSourceViewSet, basename='datasource')
+router.register(r'datasources', views.DataMoverDataSourceViewSet, basename='datamoverdatasource')
 router.register(r'datamoverconfigs', views.DataMoverConfigViewSet, basename='datamoverconfig')
 
 urlpatterns = router.urls
