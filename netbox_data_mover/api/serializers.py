6f87a3fdbf502837d85b3a7edd88f773ec643af3
Eric Hester <eric.hester@umbrella.tech>
2024-11-03 13:44:54 -0500

    fixing serializers

--- netbox_data_mover/api/serializers.py
+++ netbox_data_mover/api/serializers.py
@@ -1,12 +1,24 @@
-from netbox.api.serializers import NetBoxModelSerializer
+from rest_framework import serializers
 from ..models import DataMoverConfig, DataMoverDataSource
-
-class DataMoverConfigSerializer(NetBoxModelSerializer):
-    class Meta:
-        model = DataMoverConfig
-        fields = '__all__'
+from netbox.api.serializers import NetBoxModelSerializer
 
 class DataMoverDataSourceSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverDataSource
+        fields = '__all__'
+
+class DataMoverConfigSerializer(NetBoxModelSerializer):
+    source_api = serializers.HyperlinkedRelatedField(
+        view_name='plugins-api:netbox_data_mover:datamoverdatasource-detail',
+        queryset=DataMoverDataSource.objects.all(),
+        lookup_field='pk'
+    )
+    destination_api = serializers.HyperlinkedRelatedField(
+        view_name='plugins-api:netbox_data_mover:datamoverdatasource-detail',
+        queryset=DataMoverDataSource.objects.all(),
+        lookup_field='pk'
+    )
+
+    class Meta:
+        model = DataMoverConfig
         fields = '__all__'
--- netbox_data_mover/api/urls.py
+++ netbox_data_mover/api/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'datamoverdatasources', views.DataMoverDataSourceViewSet, basename='datasource')
+router.register(r'datasources', views.DataMoverDataSourceViewSet, basename='datasource')
 router.register(r'datamoverconfigs', views.DataMoverConfigViewSet, basename='datamoverconfig')
 
 urlpatterns = router.urls
