bb6c39131c618c22bffeee9cc29c0d6e09338173
Eric Hester <eric.hester@umbrella.tech>
2024-11-04 10:45:00 -0500

    changed api url

--- netbox_data_mover/api/urls.py
+++ netbox_data_mover/api/urls.py
@@ -7,5 +7,5 @@ router.register(r'datasources', views.DataMoverDataSourceViewSet)
 router.register(r'datamoverconfigs', views.DataMoverConfigViewSet)
 
 urlpatterns = router.urls + [
-    path('datasources/get_fields/', views.get_fields, name='datasource_fields'),
+    path('get_ds_fields/', views.get_fields, name='datasource_fields'),
 ]
