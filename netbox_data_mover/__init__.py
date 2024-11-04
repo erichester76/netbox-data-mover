0ad671b38a1e5a6b31ad675f755978abddb0193c
Eric Hester <eric.hester@umbrella.tech>
2024-11-04 08:17:33 -0500

    update version add delete triggers

--- netbox_data_mover/__init__.py
+++ netbox_data_mover/__init__.py
@@ -5,7 +5,7 @@ class NetboxDataMoverConfig(PluginConfig):
     name = "netbox_data_mover"
     verbose_name = "NetBox Data Mover"
     description = "A NetBox plugin to manage data movement configurations between various sources and destinations."
-    version = "0.0.1"
+    version = "4.1.0"
     author = "Eric Hester"
     author_email = "hester1@clemson.edu"
     base_url = "datamover"
