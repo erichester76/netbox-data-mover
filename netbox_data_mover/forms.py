28f15ad0df782a39d715deb60684e52121e16ef1
Eric Hester <eric.hester@umbrella.tech>
2024-11-04 09:19:02 -0500

    more layout fixes

--- netbox_data_mover/forms.py
+++ netbox_data_mover/forms.py
@@ -5,16 +5,30 @@ from .models import DataMoverConfig, DataMoverDataSource
 class DataMoverConfigForm(forms.ModelForm):
     class Meta:
         model = DataMoverConfig
-        fields = ['name', 'description', 'schedule', 'source', 'source_endpoint', 'destination', 'destination_endpoint']
+        fields = ['name', 'schedule', 'description', 'source', 'source_endpoint', 'destination', 'destination_endpoint']
         widgets = {
             'source': forms.Select(attrs={'class': 'form-select'}),
             'source_endpoint': forms.Select(attrs={'class': 'form-select'}),
             'destination': forms.Select(attrs={'class': 'form-select'}),
             'destination_endpoint': forms.Select(attrs={'class': 'form-select'}),
         }
+        SCHEDULE_CHOICES = [
+            ('0 * * * *', 'Hourly'),
+            ('0 0 * * *', 'Daily'),
+            ('0 0 * * 0', 'Weekly'),
+            ('0 0 1 * *', 'Monthly'),
+        ]
+
+        name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control d-inliune-block col-md-6'}))
+        schedule = forms.ChoiceField(
+        choices=SCHEDULE_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select d-inliune-block col-md-6'})
+    )
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields[]
         self.fields['source'].widget.attrs.update({'class': 'form-select d-inline-block col-md-6'})
         self.fields['source_endpoint'].widget.attrs.update({'class': 'form-select d-inline-block col-md-6'})
         self.fields['destination'].widget.attrs.update({'class': 'form-select d-inline-block col-md-6'})
