a706057562da4e164bf44efa164e9bea1de1e456
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 11:06:04 -0500

    moved things back to right place

--- netbox_data_mover/forms.py
+++ netbox_data_mover/forms.py
@@ -4,54 +4,61 @@ from .models import DataMoverConfig, DataMoverDataSource
 from utilities.forms.fields import DynamicModelChoiceField
 
 class DataMoverConfigForm(NetBoxModelForm):
+    
+    SCHEDULE_CHOICES = [
+        ('', 'None - Manual Only'),
+        ('0 * * * *', 'Hourly'),
+        ('0 0 * * *', 'Daily'),
+        ('0 0 * * 0', 'Weekly'),
+        ('0 0 1 * *', 'Monthly'),
+    ]
+
+    source = DynamicModelChoiceField(
+        label=_('Source'),
+        queryset=DataMoverDataSource.objects.all(),
+        required=True,
+    )
+
+    destination = DynamicModelChoiceField(
+        label=_('Destination'),
+        queryset=DataMoverDataSource.objects.all(),
+        required=True,
+    )
+
+    source_endpoint = DynamicModelChoiceField(
+        label=_('Source Endpoint'),
+        queryset=DataMoverDataSource.objects.none(),
+        required=True,
+        query_params={'datamoverdatasourceid': '$source', 'type': 'endpoints'},
+    )
+
+    destination_endpoint = DynamicModelChoiceField(
+        label=_('Destination Endpoint'),
+        queryset=DataMoverDataSource.objects.none(),
+        required=True,
+        query_params={'datamoverdatasourceid': '$destination', 'type': 'endpoints'},
+    )
+
     class Meta:
-        
-        SCHEDULE_CHOICES = [
-            ('','None - Manual Only'),
-            ('0 * * * *', 'Hourly'),
-            ('0 0 * * *', 'Daily'),
-            ('0 0 * * 0', 'Weekly'),
-            ('0 0 1 * *', 'Monthly'),
-        ]  
         model = DataMoverConfig
-        fields = ['name', 'schedule', 'description', 'source', 'source_endpoint', 'destination', 'destination_endpoint']
-         
+        fields = [
+            'name', 'schedule', 'description', 
+            'source', 'source_endpoint', 'destination', 'destination_endpoint'
+        ]
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control d-inline-block col-md-6'}),
             'schedule': forms.Select(choices=SCHEDULE_CHOICES, attrs={'class': 'form-select d-inline-block col-md-6'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
-        
-        source = DynamicModelChoiceField(
-            label=_('Source'),
-            queryset=DataMoverDataSource.objects.all(),
-            required=True,
-        )
-
-        destination = DynamicModelChoiceField(
-            label=_('Destination'),
-            queryset=DataMoverDataSource.objects.all(),
-            required=True,
-        )
-        
-        source_endpoint = DynamicModelChoiceField(
-            label=_('Source Endpoint'),
-            queryset=DataMoverDataSource.objects.none(),
-            required=True,
-            query_params={'datamoverdatasourceid': '$source', 'type': 'endpoints'},
-        )
-
-        destination_endpoint = DynamicModelChoiceField(
-            label=_('Destination Endpoint'),
-            queryset=DataMoverDataSource.objects.none(),
-            required=True,
-            query_params={'datamoverdatasourceid': '$destination', 'type': 'endpoints'},
-        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
 class DataMoverDataSourceForm(NetBoxModelForm):
     class Meta:
         model = DataMoverDataSource
-        fields = ['name', 'type', 'module', 'endpoints', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
+        fields = [
+            'name', 'type', 'module', 'endpoints', 'auth_method', 'auth_function', 
+            'find_function', 'create_function', 'update_function', 'fetch_function', 
+            'auth_args', 'base_urls'
+        ]
