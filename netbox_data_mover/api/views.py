b9f6eab848812d17b039783b30a343bdd79caf5a
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 16:35:45 -0500

    .

--- netbox_data_mover/api/views.py
+++ netbox_data_mover/api/views.py
@@ -12,19 +12,19 @@ class DataMoverDataSourceViewSet(viewsets.ModelViewSet):
    
     def list(self, request, *args, **kwargs):
         
-        if request.query_params.get('endpoint_id'):
-            try:
-                datasource = self.queryset.get(request.query_params.get('endpoint_id'))
-                endpoints = datasource.endpoints.split(',')
-                return Response({'results': [{'id': idx, 'display': endpoint.strip()} for idx, endpoint in enumerate(endpoints)]})
-            except DataMoverDataSource.DoesNotExist:
-                return Response({'results': []})
-        if request.query_params.get('fields_id'):
-            try:
-                datasource = self.queryset.get(request.query_params.get('fields_id'))
-                return Response({'results': [{'id': 1, 'display': 'Test 123'}]})
-            except DataMoverDataSource.DoesNotExist:
-                return Response({'results': []})
+        # if request.query_params.get('endpoint_id'):
+        #     try:
+        #         datasource = self.queryset.get(request.query_params.get('endpoint_id'))
+        #         endpoints = datasource.endpoints.split(',')
+        #         return Response({'results': [{'id': idx, 'display': endpoint.strip()} for idx, endpoint in enumerate(endpoints)]})
+        #     except DataMoverDataSource.DoesNotExist:
+        #         return Response({'results': []})
+        # if request.query_params.get('fields_id'):
+        #     try:
+        #         datasource = self.queryset.get(request.query_params.get('fields_id'))
+        #         return Response({'results': [{'id': 1, 'display': 'Test 123'}]})
+        #     except DataMoverDataSource.DoesNotExist:
+        #         return Response({'results': []})
 
         # Otherwise, return the full DataMoverDataSource records.
         return super().list(request, *args, **kwargs)
