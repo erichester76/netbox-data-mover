956fd7937caf91899be170b013a5b27ef3ddd74c
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 11:40:40 -0500

    .

--- netbox_data_mover/api/serializers.py
+++ netbox_data_mover/api/serializers.py
@@ -5,14 +5,14 @@ from netbox.api.serializers import NetBoxModelSerializer
 class DataMoverDataSourceSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverDataSource
-        fields = ['id', 'name', 'type', 'module', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
+        fields = ['id', 'display', 'display_url', 'name', 'type', 'module', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
 
 class DataMoverConfigSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverConfig
-        fields = ['id', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
+        fields = ['id', 'display', 'display_url', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
 
 class DataSourceFieldsSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverDataSource
-        fields = ['id', 'name', 'module', 'auth_function']
+        fields = ['id', 'display', 'display_url', 'name', 'module', 'auth_function']
