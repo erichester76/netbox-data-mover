d01e4e2daca45ac2611f54b493d5991e1b0b3f5c
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 18:48:01 -0500

    .

--- netbox_data_mover/forms.py
+++ netbox_data_mover/forms.py
@@ -7,49 +7,51 @@ from utilities.forms.fields import DynamicModelChoiceField
 
 class DataMoverConfigForm(NetBoxModelForm):
     
+    
+    source = DynamicModelChoiceField(
+            queryset=DataMoverDataSource.objects.all(),
+            required=True,
+            label="Source",
+            help_text="Select the data source."
+    )
+    
+    destination = DynamicModelChoiceField(
+        queryset=DataMoverDataSource.objects.all(),
+        required=True,
+        label="Destination",
+        help_text="Select the data destination."
+    )
+    
+    source_endpoint = DynamicModelChoiceField(
+        queryset=DataMoverDataSource.objects.none(),
+        required=True,
+        label="Source Endpoint",
+        query_params={'endpoint_id': '$source', 'type': 'endpoints'},
+        help_text="Select an endpoint for the chosen source."
+    )
+
+    destination_endpoint = DynamicModelChoiceField(
+        queryset=DataMoverDataSource.objects.none(),
+        required=True,
+        label="Destination Endpoint",
+        query_params={'endpoint_id': '$destination', 'type': 'endpoints'},
+        help_text="Select an endpoint for the chosen destination."
+    )
+
+    
     class Meta:
         SCHEDULE_CHOICES = [
-        ('', 'None - Manual Only'),
-        ('0 * * * *', 'Hourly'),
-        ('0 0 * * *', 'Daily'),
-        ('0 0 * * 0', 'Weekly'),
-        ('0 0 1 * *', 'Monthly'),
+            ('', 'None - Manual Only'),
+            ('0 * * * *', 'Hourly'),
+            ('0 0 * * *', 'Daily'),
+            ('0 0 * * 0', 'Weekly'),
+            ('0 0 1 * *', 'Monthly'),
         ]
         
         model = DataMoverConfig
         fields = ['source', 'source_endpoint', 'destination', 'destination_endpoint', 'name', 'schedule', 'description']
         
-        source = DynamicModelChoiceField(
-            queryset=DataMoverDataSource.objects.all(),
-            required=True,
-            label="Source",
-            help_text="Select the data source."
-        )
         
-        destination = DynamicModelChoiceField(
-            queryset=DataMoverDataSource.objects.all(),
-            required=True,
-            label="Destination",
-            help_text="Select the data destination."
-        )
-        
-        source_endpoint = DynamicModelChoiceField(
-            queryset=DataMoverDataSource.objects.none(),
-            required=True,
-            label="Source Endpoint",
-            query_params={'endpoint_id': '$source', 'type': 'endpoints'},
-            help_text="Select an endpoint for the chosen source."
-        )
-
-        destination_endpoint = DynamicModelChoiceField(
-            queryset=DataMoverDataSource.objects.none(),
-            required=True,
-            label="Destination Endpoint",
-            query_params={'endpoint_id': '$destination', 'type': 'endpoints'},
-            help_text="Select an endpoint for the chosen destination."
-        )
-
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
