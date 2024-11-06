69977f7326099389598db7735c0c4b3658578c0a
Eric Hester <eric.hester@umbrella.tech>
2024-11-05 23:39:48 -0500

    .

--- netbox_data_mover/forms.py
+++ netbox_data_mover/forms.py
@@ -4,15 +4,13 @@ from utilities.forms.fields import DynamicModelChoiceField
 
 class DataMoverConfigForm(forms.ModelForm):
     class Meta:
-       
         SCHEDULE_CHOICES = [
             ('','None - Manual Only'),
             ('0 * * * *', 'Hourly'),
             ('0 0 * * *', 'Daily'),
             ('0 0 * * 0', 'Weekly'),
             ('0 0 1 * *', 'Monthly'),
-        ]
-        
+        ]  
         model = DataMoverConfig
         fields = ['name', 'schedule', 'description', 'source', 'source_endpoint', 'destination', 'destination_endpoint']
         widgets = {
@@ -49,7 +47,6 @@ class DataMoverConfigForm(forms.ModelForm):
         self.fields['destination_endpoint'].choices = []  # To be dynamically populated
     
 class DataMoverDataSourceForm(forms.ModelForm):
-
     class Meta:
         model = DataMoverDataSource
         fields = ['name', 'type', 'module', 'endpoints', 'auth_method', 'auth_function', 'find_function', 'create_function', 'update_function', 'fetch_function', 'auth_args', 'base_urls']
