c2c1fb6562c2e74c8c9f339e72b5aff90b1f354b
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 20:03:00 -0500

    .

--- netbox_data_mover/api/serializers.py
+++ netbox_data_mover/api/serializers.py
@@ -2,23 +2,23 @@ from ..models import DataMoverConfig, DataMoverDataSource
 from netbox.api.serializers import NetBoxModelSerializer
 from rest_framework import serializers
 
-# class EndpointSerializer(NetBoxModelSerializer):
-#     id = serializers.IntegerField()
-#     display = serializers.CharField()
-#     name = serializers.CharField()
-#     slug = serializers.CharField()
-#     description = serializers.CharField()
+class EndpointSerializer(NetBoxModelSerializer):
+    id = serializers.IntegerField()
+    display = serializers.CharField()
+    name = serializers.CharField()
+    slug = serializers.CharField()
+    description = serializers.CharField()
 
 class DataMoverDataSourceSerializer(NetBoxModelSerializer):
-    #endpoints = EndpointSerializer(source='get_endpoints', many=True, read_only=True)
+    endpoints = EndpointSerializer(source='get_endpoints', many=True, read_only=True)
 
     class Meta:
         model = DataMoverDataSource
-        fields = ['id', 'name', 'type', 'module', 'auth_method', 'auth_function', 'endpoints']
+        fields = ['id', 'name', 'display', 'type', 'module', 'auth_method', 'auth_function', 'endpoints']
         brief_fields=  ['id', 'display', 'name']
       
 class DataMoverConfigSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverConfig
-        fields = ['id', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
+        fields = ['id', 'name', 'display', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
         brief_fields = ['id', 'display', 'name']
