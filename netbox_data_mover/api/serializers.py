21dea8b55b8494cbfff7a51e3bb15a05ba49222a
Eric Hester <eric.hester@umbrella.tech>
2024-11-06 19:57:50 -0500

    .

--- netbox_data_mover/api/serializers.py
+++ netbox_data_mover/api/serializers.py
@@ -15,8 +15,10 @@ class DataMoverDataSourceSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverDataSource
         fields = ['id', 'name', 'type', 'module', 'auth_method', 'auth_function', 'endpoints']
+        brief_fields=  ['id', 'display', 'name']
       
 class DataMoverConfigSerializer(NetBoxModelSerializer):
     class Meta:
         model = DataMoverConfig
-        fields = ['id', 'display', 'display_url', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
+        fields = ['id', 'name', 'description', 'schedule', 'source', 'destination', 'last_run_records_changed', 'last_run_status', 'last_run_time']
+        brief_fields = ['id', 'display', 'name']
